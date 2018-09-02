a97bfae440f192ec576568ee72216244036aaeeb
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-09-02 11:15:35 +0300

    my cafe

--- src/Menu.java
+++ src/Menu.java
@@ -4,9 +4,6 @@ public class Menu {
     private ArrayList<String> dishes = new ArrayList<>();
 
     public ArrayList<String> getDishes() {
-        dishes.add(0, "hot dog");
-        dishes.add(1, "hamburger");
-        dishes.add(2, "Orange juice");
         return dishes;
     }
 
