2edbc704bfbeb302ec9ed6a5a97f88a7c2dbb383
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-09-05 16:39:35 +0300

    my cafe

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu();
         CafeSwallow swallow = new CafeSwallow();
         swallow.chef();
     }
