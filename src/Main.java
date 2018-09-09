4ea1a9ff7e4f7a74678e8479f7ea53f8ec8bb578
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-09-09 16:11:50 +0300

    my cafe

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         CafeSwallow swallow = new CafeSwallow(createTables());
         swallow.printTable();
+        BookingATable();
     }
 
     public static void createMenu() {
@@ -21,5 +23,15 @@ public class Main {
         tables.add(new Table(2, 2, true));
         tables.add(new Table(3, 3, false));
         return tables;
+    }
+
+    public static void BookingATable() {
+        CafeSwallow swallow = new CafeSwallow(createTables());
+        List<Table>tables = new ArrayList<>();
+        System.out.println("For what table would you sit? Enter the table number:");
+        Scanner scan = new Scanner(System.in);
+        int id1 = scan.nextInt();
+        int id2 = scan.nextInt();
+        int id3 = scan.nextInt();
     }
 }
